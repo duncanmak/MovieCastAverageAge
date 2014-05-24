8e41d8b130bd537c62c81d4cdfeb6739d8664ac4
Duncan Mak <duncan.mak@xamarin.com>
2014-05-24 00:54:24 -0400

    Move the callback-stytle code into its own file

--- app.ts
+++ app.ts
@@ -1,64 +1,19 @@
-﻿import _ = require('underscore');
-import async = require('async');
-import rt = require('./rotten-tomatoes');
+﻿import callbacks = require('./callbacks');
 import readline = require('readline');
-import secret = require('./secret')
-import tmdb = require('./tmdb');
-import util = require('util');
 
-function testRottenTomatoes(n) {
-    var rotten = new rt.RottenTomatoes(secret.ROTTENTOMATOES_KEY);
-    rotten.getBoxOfficeMovies((err, movies) => {
-        movies.forEach(m => {
-            console.log(util.format("%s: %s", m.title, m.abridged_cast.map(c => c.name)));
-        }, n);
-    });
+interface App {
+    testTheMovieDB?(string): void
+    testRottenTomatoes?(string): void
+    testEverything?(number): void
 }
 
-function testTheMovieDB(name: string) {
-    var t = new tmdb.TMDB(secret.TMDB_KEY);
-    t.getAge(name, age => {
-        console.log("%s is %d years old.", name, age);
-    });
-}
-
-function getAge(_tmdb: tmdb.TMDB, name: string, callback: Function) {
-    _tmdb.getAge(name, age => callback(null, { name: name, age: age }))
-}
-
-function getMovies(_rt: rt.RottenTomatoes, n: number, callback: (error: any, movies: rt.Movie[]) => any) {
-    _rt.getBoxOfficeMovies(callback, n);
-}
-
-function testEverything(n: number) {
-    var _rt = new rt.RottenTomatoes(secret.ROTTENTOMATOES_KEY);
-    var _tmdb = new tmdb.TMDB(secret.TMDB_KEY);
-
-    async.parallel(
-        [async.apply(getMovies, _rt, n)],
-        (err, movies) => {
-            async.parallel(
-                _.flatten(movies).map(
-                    movie => ignored => {
-                        async.parallel(
-                            movie.abridged_cast.map(c => async.apply(getAge, _tmdb, c.name)),
-                            (error, results) => {
-                                console.log("Results for " + movie.title + ":\n");
-                                results.forEach(x => console.log("%s is %d years old.", x.name, x.age));
-                                var ages = results.map(each => each.age);
-                                console.log(util.format("The average age is %d.", (ages.reduce((a, b) => a + b) / ages.length)));
-                                console.log();
-                            });
-                    }),
-                (error, result) => { })
-        });
-};
+var app: App = callbacks;
 
 console.log("Hello World!");
 
-// testRottenTomatoes(1);
-// testTheMovieDB("Tom Cruise");
+app.testRottenTomatoes(1);
+app.testTheMovieDB("Tom Cruise");
 
-testEverything(3);
+app.testEverything(3);
 
 readline.createInterface({input: process.stdin, output: process.stdout}).prompt();
---
+++ callbacks.ts
@@ -0,0 +1,54 @@
+﻿import _ = require('underscore');
+import async = require('async');
+import rt = require('./rotten-tomatoes');
+import secret = require('./secret')
+import tmdb = require('./tmdb');
+import util = require('util');
+
+export function testRottenTomatoes(n) {
+    var rotten = new rt.RottenTomatoes(secret.ROTTENTOMATOES_KEY);
+    rotten.getBoxOfficeMovies((err, movies) => {
+        movies.forEach(m => {
+            console.log(util.format("%s: %s", m.title, m.abridged_cast.map(c => c.name)));
+        }, n);
+    });
+}
+
+export function testTheMovieDB(name: string) {
+    var t = new tmdb.TMDB(secret.TMDB_KEY);
+    t.getAge(name, age => {
+        console.log("%s is %d years old.", name, age);
+    });
+}
+
+function getAge(_tmdb: tmdb.TMDB, name: string, callback: Function) {
+    _tmdb.getAge(name, age => callback(null, { name: name, age: age }))
+}
+
+function getMovies(_rt: rt.RottenTomatoes, n: number, callback: (error: any, movies: rt.Movie[]) => any) {
+    _rt.getBoxOfficeMovies(callback, n);
+}
+
+export function testEverything(n: number) {
+    var _rt = new rt.RottenTomatoes(secret.ROTTENTOMATOES_KEY);
+    var _tmdb = new tmdb.TMDB(secret.TMDB_KEY);
+
+    async.parallel(
+        [async.apply(getMovies, _rt, n)],
+        (err, movies) => {
+            async.parallel(
+                _.flatten(movies).map(
+                    movie => ignored => {
+                        async.parallel(
+                            movie.abridged_cast.map(c => async.apply(getAge, _tmdb, c.name)),
+                            (error, results) => {
+                                console.log("Results for " + movie.title + ":\n");
+                                results.forEach(x => console.log("%s is %d years old.", x.name, x.age));
+                                var ages = results.map(each => each.age);
+                                console.log(util.format("The average age is %d.", (ages.reduce((a, b) => a + b) / ages.length)));
+                                console.log();
+                            });
+                    }),
+                (error, result) => { })
+        });
+};
